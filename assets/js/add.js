500b426cf7d5c2f9fed19e81a5368f6e65908598
javohir <ajavohir969@gmail.com>
2024-02-01 23:15:25 +0500

    menu

---
+++ assets/js/add.js
@@ -0,0 +1,7 @@
+let ism = prompt("Ismingizni kiriting");
+var familiya = prompt("Familiyangizni kiriting");
+const yosh = prompt("Yoshingizni kiriting");
+
+console.log(
+    "Salom mening ismim " + ism + " familyam " + familiya + " yoshim " + yosh + "da."
+)
